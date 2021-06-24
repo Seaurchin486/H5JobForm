75d624dbd489fab986cff68c9901412dcdb0531b
海胆 <8065350+seaurchin486@user.noreply.gitee.com>
2021-06-24 09:42:08 +0800

    onclick

---
+++ js/click.js
@@ -0,0 +1,23 @@
+function datepickShow() {
+    document.querySelector(".datepick").style.display = "block";
+    document.querySelector("#chosed_box").style.display = "block";
+}
+function marrypickShow() {
+    document.querySelector(".marrypick").style.display = "block";
+    document.querySelector("#chosed_box").style.display = "block";
+}
+
+
+function datepickHide() {
+    document.querySelector(".datepick").style.display = "none";
+    document.querySelector("#chosed_box").style.display = "none";
+}
+function marrypickHide() {
+    document.querySelector(".marrypick").style.display = "none";
+    document.querySelector("#chosed_box").style.display = "none";
+}
+function allHide() {
+    document.querySelector(".datepick").style.display = "none";
+    document.querySelector(".marrypick").style.display = "none";
+    document.querySelector("#chosed_box").style.display = "none";
+}
